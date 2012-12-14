ff8f373488198d4dcf7083df1064302862857f88
Annor Theophilus <fhim50@live.com>
2012-12-14 14:20:40 +0000

    simple_report admin  done

--- simple_report/models.py
+++ simple_report/models.py
@@ -1,12 +1,67 @@
 from django.db import models
-class student(models.Model):
-	name = models.CharField(max_length=50)
-	course = models.CharField(20)
-
-
-
+from django.contrib import admin
 
 
 
 class Report(models.Model):
-# Create your models here.
+	COURSE = (("BUSINESS","BUSINESS"),("SCIENCE","SCIENCE"))
+
+
+	name = models.CharField("Student Name",max_length=50)
+	course = models.CharField( "Student Cource",choices = COURSE , max_length = 20 , default = COURSE[0][0])
+	
+	def __unicode(self):
+		return self.name
+
+class Subject(models.Model):
+	
+	GRADE = (
+				("A","Grade A"),
+				("B" , "Grade B"),
+				("C", "Grade C"),
+				("D", "Grade D"),
+				("E" ,"Grade E"),
+				("F" , "Grade F"),
+			)
+	
+	
+	
+	title = models.CharField(max_length = 20)
+	grade = models.CharField(max_length = 1 , choices = GRADE ,default = GRADE[0][0])
+	sudject = models.ForeignKey(Report)
+	
+	def __unicode__(self):
+		 return self.title
+		 
+	def save(self):
+		self.title = "maths"
+		super(Subject , self).save()
+	 
+
+'''	
+	def create(self):
+		core_subjects = ['Eng' , 'Math' , 'Sci' , 'SoS']
+		for i in core_subject:
+			b= 
+'''		
+
+class SubjectInline(admin.TabularInline): 
+	model = Subject
+	extra = 8
+		 
+class ReportAdmin(admin.ModelAdmin):
+	inlines=[SubjectInline]
+	list_display = ["name" , "course"]
+	
+class SubjectAdmin(admin.ModelAdmin):
+	extra = 8
+		
+
+
+
+
+admin.site.register(Report , ReportAdmin)		
+admin.site.register(Subject , SubjectAdmin)		
+		 
+		 
+
--- smsreportcard/settings.py
+++ smsreportcard/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     #'south',
 	'registration', 
 	'reg',
+	'simple_report',
 
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
