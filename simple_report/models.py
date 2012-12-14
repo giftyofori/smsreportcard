4d1b1b858c20944239e31b5234d92bdb234ff9da
Maame <maame@maame-HP-G62-Notebook-PC.(none)>
2012-12-14 12:15:23 +0000

    just added

--- simple_report/models.py
+++ simple_report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+class student(models.Model):
+	name = models.CharField(max_length=50)
+	course = models.CharField(20)
 
+
+
+
+
+
+class Report(models.Model):
 # Create your models here.
